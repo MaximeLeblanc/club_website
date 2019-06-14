e4491daab28878d4bd72d4dcf795e0d3b277d91d
Unknown <leblanc.maxime49@hotmail.fr>
2019-06-14 22:50:39 +0200

    [WIP] profile pages photo

--- public/js/administration/profile.js
+++ public/js/administration/profile.js
@@ -1,18 +1,62 @@
 $(function() {
     $url = $('#photoUrl').val();
-    $croppedImage = $('#photo').croppie({
-        viewport: {
-            width: 100,
-            height: 100,
-            type: 'circle'
-        },
-        boundary: {
-            width: 150,
-            height: 150
+    $croppedImage = "";
+    
+    if ($url != '') {
+        $croppedImage = $('#photo').croppie({
+            viewport: {
+                width: 100,
+                height: 100,
+                type: 'circle'
+            },
+            boundary: {
+                width: 150,
+                height: 150
+            }
+        });
+        $croppedImage.croppie('bind', {
+            url: $url,
+            zoom: 0.01,
+        });
+        $('#deleteProfileImage').removeClass('disabled');
+    }
+
+    $('#adminImageInput').change(function() {
+        readFile(this);
+    });
+
+    $('#deleteProfileImage').click(function() {
+        if (!$(this).hasClass('disabled')) {
+            $croppedImage.croppie('destroy');
+            $croppedImage = "";
+            $(this).addClass('disabled');
         }
-    });
-    $croppedImage.croppie('bind', {
-        url: $url,
-        zoom: 0.01,
-    });
+
+    })
 });
+
+function readFile(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            if ($croppedImage == "") {
+                $croppedImage = $('#photo').croppie({
+                    viewport: {
+                        width: 100,
+                        height: 100,
+                        type: 'circle'
+                    },
+                    boundary: {
+                        width: 150,
+                        height: 150
+                    }
+                });
+            }
+            $croppedImage.croppie('bind', {
+                url: e.target.result,
+            });
+            $('#deleteProfileImage').removeClass('disabled');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
